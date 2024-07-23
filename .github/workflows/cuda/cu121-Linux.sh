5e95651b2adb4c09f25d2310573a4f73bcccb938
suncloudsmoon <34616349+suncloudsmoon@users.noreply.github.com>
2024-07-23 16:00:39 -0700

    Create cu121-Linux.sh

---
+++ .github/workflows/cuda/cu121-Linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Strip the periods from the version number
+OS_VERSION=$(echo $(lsb_release -sr) | tr -d .)
+OS=ubuntu${OS_VERSION}
+
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
+sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget -nv https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-${OS}-12-1-local_12.1.1-530.30.02-1_amd64.deb
+
+sudo dpkg -i cuda-repo-${OS}-12-1-local_12.1.1-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-${OS}-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+sudo apt-get -qq update
+sudo apt install cuda cuda-nvcc-12-1 cuda-libraries-dev-12-1
+sudo apt clean
+
+rm -f https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-${OS}-12-1-local_12.1.1-530.30.02-1_amd64.deb
